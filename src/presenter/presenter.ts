8874726a2316b06d2eb86473ac8b1353c248a609
Minh <vietminhle98@gmail.com>
2021-04-23 10:50:13 +1000

    add presenter.present

--- src/presenter/presenter.ts
+++ src/presenter/presenter.ts
@@ -1,13 +1,37 @@
+import findAndReplaceDOMText from 'findandreplacedomtext';
 import { ProxyRecord } from 'storage/types';
-
-export function replaceProxy(record: ProxyRecord) {
-  const { label, value } = record;
-}
 
 export class Presenter {
   constructor(
-    private readonly document: Document,
     private readonly scope: HTMLElement,
-    private readonly Observer: MutationObserver
+    private readonly Observer: typeof MutationObserver
   ) {}
+
+  present(proxies: ProxyRecord[]) {
+    this.#replaceAll(proxies);
+    const observer = this.#createObserver(proxies);
+    observer.observe(this.scope, { childList: true, subtree: true });
+  }
+
+  #replace(proxy: ProxyRecord, scope = this.scope) {
+    findAndReplaceDOMText(scope, {
+      find: new RegExp(proxy.label),
+      replace: proxy.value,
+    });
+  }
+
+  #replaceAll(proxies: ProxyRecord[], scope = this.scope) {
+    proxies.forEach(proxy => this.#replace(proxy, scope));
+  }
+
+  #createObserver(proxies: ProxyRecord[]) {
+    const observer = new this.Observer(mutations => {
+      mutations.forEach(mutation => {
+        mutation.addedNodes.forEach(node => {
+          this.#replaceAll(proxies, node as any);
+        });
+      });
+    });
+    return observer;
+  }
 }
